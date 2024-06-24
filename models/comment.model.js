5cb5faf03bcfc7d5004f2e62e6547619b5a74b6e
Huy Tuan <tuanthhe172693@fpt.edu.vn>
2024-06-24 19:51:13 +0700

    feat: Be-Update-models

--- models/comment.model.js
+++ models/comment.model.js
@@ -1,31 +1,31 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const CommentSchema = new Schema(
-  {
-    photo: {
-      type: Schema.Types.ObjectId,
-      ref: "photo",
-      required: true,
+    {
+        photo: {
+            type: Schema.Types.ObjectId,
+            ref: 'photo',
+            required: true,
+        },
+        user: {
+            type: Schema.Types.ObjectId,
+            ref: 'user',
+            required: true,
+        },
+        content: {
+            type: String,
+            required: true,
+            maxLength: [200, 'Content must be at most 200 characters'],
+        },
+        status: {
+            type: String,
+            enum: ['ACTIVE', 'INACTIVE', 'DELETED'],
+            default: 'ACTIVE',
+        },
     },
-    user: {
-      type: Schema.Types.ObjectId,
-      ref: "user",
-      required: true,
-    },
-    content: {
-      type: String,
-      required: true,
-      maxLength: [200, "Content must be at most 200 characters"],
-    },
-    status: {
-      type: String,
-      enum: ["ACTIVE", "INACTIVE", "DELETED"],
-      default: "ACTIVE",
-    },
-  },
-  { timestamps: true }
+    { timestamps: true }
 );
 
-const Comment = mongoose.model("comment", CommentSchema);
+const Comment = mongoose.model('comment', CommentSchema);
 module.exports = Comment;
--- models/history.model.js
+++ models/history.model.js
@@ -1,23 +1,26 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const HistorySchema = new Schema({
-    user: {
-        type: Schema.Types.ObjectId,
-        ref: 'user',
-        required: true,
+const HistorySchema = new Schema(
+    {
+        user: {
+            type: Schema.Types.ObjectId,
+            ref: 'user',
+            required: true,
+        },
+        actionType: {
+            type: String,
+            required: true,
+            enum: ['CREATE', 'UPDATE', 'DELETE', 'COMMENT', 'REACT'],
+        },
+        photo: {
+            type: Schema.Types.ObjectId,
+            ref: 'photo',
+            required: true,
+        },
     },
-    actionType: {
-        type: String,
-        required: true,
-        enum: ['CREATE', 'UPDATE', 'DELETE', 'COMMENT', 'REACT'],
-    },
-    photo: {
-        type: Schema.Types.ObjectId,
-        ref: 'photo',
-        required: true,
-    },
-});
+    { timestamps: true }
+);
 
 const History = mongoose.model('history', HistorySchema);
 module.exports = History;
